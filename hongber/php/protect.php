fa95ddf26894cd111417c1c99fc13e531337c53b
SeoEunGi0802 <lolove08@naver.com>
2021-05-24 23:17:41 +0900

    add project.php

--- hongber/php/protect.php
+++ hongber/php/protect.php
@@ -1,12 +1,12 @@
 <?php
-$config = 'C:\Bitnami\wampstack-8.0.3-1\php\extras\ssl';
+$config = 'hongber\openssl.cnf';
 
+// 키 생성
 $genRes = openssl_pkey_new(array(
     'config' => $config,
     'digest_alg' => "sha512",
     'private_key_bits' => 4096,
     'private_key_type' => OPENSSL_KEYTYPE_RSA,
-
 ));
 
 // 개인키 생성
@@ -17,14 +17,14 @@ $pubKeyArray = openssl_pkey_get_details($genRes);
 $pubKey = $pubKeyArray['key'];
 
 // 암호화
-$plainText = "암호화 될 문자열";
+$plainText = "lolove08@";
 openssl_public_encrypt($plainText, $encrypted, $pubKey);
 $encryptedBase64 = base64_encode($encrypted);
 
 // 복호화
 openssl_private_decrypt(base64_decode($encryptedBase64), $decrypted, $privKey);
 
-echo "평문 : " . $plainText . "<br />";
-echo "암호화 : " . $encryptedBase64 . "<br />";
-echo "복호화 : " . $decrypted . "<br />";
-?>
+echo "평문 : " . $plainText . "<br><br>";
+echo "암호화 : " . "<p style='word-break:break-all'>" . $encryptedBase64 . "</p>";
+echo "복호화 : " . $decrypted . "<br><br>";
+echo "공개키 : " . "<p style='word-break:break-all'>" . $pubKey . "</p>";
